39c376c005e75520b508c49b8eb20ad81e32b4cb
אהרן פלדמן <aronfldmn@gmail.com>
2026-04-14 00:50:52 +0000

    File changes

--- src/lib/hebrewDateUtils.js
+++ src/lib/hebrewDateUtils.js
@@ -169,7 +169,7 @@ export async function fetchZmanim(date, lat, lng, tzid, candleMinutes = 18) {
 
   const result = {
     alotHaShachar: t(z.Alos72),
-    zmanTzitzit: t(z.Misheyakir11Point5Degrees || z.Misheyakir),
+    zmanTzitzit: t(z.Misheyakir10Point2Degrees),
     sunrise: t(z.Sunrise),
     sofShmaGRA: t(z.SofZmanShmaGRA),
     sofShmaMA: t(z.SofZmanShmaMGA),
