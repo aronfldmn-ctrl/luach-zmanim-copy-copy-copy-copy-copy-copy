2a4df24926fe2eef86c675abc20d6e9b31fd2e79
אהרן פלדמן <aronfldmn@gmail.com>
2026-04-14 03:15:20 +0000

    File changes

--- src/lib/dafYomi.js
+++ src/lib/dafYomi.js
@@ -1,20 +1,3 @@
-import { YomiCalculator } from 'kosher-zmanim/dist/kosher-zmanim.js';
-
 export async function fetchDafYomi(date) {
-  try {
-    const dayOfDaf = YomiCalculator.getDayOfDaf(date);
-    const masechta = YomiCalculator.getMasechta(dayOfDaf);
-    const dafPage = YomiCalculator.getDaf(dayOfDaf);
-    
-    return {
-      ref: `${masechta} ${dafPage}`,
-      display: `${masechta} ${dafPage}`,
-      masechta,
-      dafPage,
-      dayOfDaf
-    };
-  } catch (error) {
-    console.error('Daf Yomi fetch error:', error);
-    return null;
-  }
+  return null;
 }
